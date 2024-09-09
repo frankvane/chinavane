73aabb9164de00de2426063d01159cc387036a8c
vane <13525442@qq.com>
2024-09-09 23:11:31 +0800

    feat: compress css js

--- js/color-modes.js
+++ js/color-modes.js
@@ -1,1 +1,109 @@
-(()=>{"use strict";const getStoredTheme=()=>localStorage.getItem("theme")||"dark";const setStoredTheme=(theme)=>localStorage.setItem("theme",theme);const setThemeIcon=(theme)=>{const iconPathPrefix=`/images/${theme}/`;const updateIcons=(className,iconName)=>{const icons=document.getElementsByClassName(className);for(const icon of icons){icon.src=iconPathPrefix+iconName}};updateIcons("right-icon","right.svg");updateIcons("clock-icon","clock.svg");updateIcons("chapter-icon","chapter.svg");updateIcons("download-icon","download.svg")};const getPreferredTheme=()=>{const storedTheme=getStoredTheme();setThemeIcon(storedTheme);if(storedTheme){return storedTheme}return"dark"};const setTheme=(theme)=>{if(theme==="auto"){document.documentElement.setAttribute("data-bs-theme",window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light")}else{document.documentElement.setAttribute("data-bs-theme",theme)}setThemeIcon(theme)};setTheme(getPreferredTheme());const showActiveTheme=(theme,focus=false)=>{const themeSwitcher=document.querySelector("#bd-theme");if(!themeSwitcher){return}const themeSwitcherText=document.querySelector("#bd-theme-text");const activeThemeIcon=document.querySelector(".theme-icon-active use");const btnToActive=document.querySelector(`[data-bs-theme-value="${theme}"]`);const svgOfActiveBtn=btnToActive.querySelector("svg use").getAttribute("href");document.querySelectorAll("[data-bs-theme-value]").forEach((element)=>{element.classList.remove("active");element.setAttribute("aria-pressed","false")});btnToActive.classList.add("active");btnToActive.setAttribute("aria-pressed","true");activeThemeIcon.setAttribute("href",svgOfActiveBtn);const themeSwitcherLabel=`${themeSwitcherText.textContent}(${btnToActive.dataset.bsThemeValue})`;themeSwitcher.setAttribute("aria-label",themeSwitcherLabel);if(focus){themeSwitcher.focus()}};window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change",()=>{const storedTheme=getStoredTheme();if(storedTheme!=="light"&&storedTheme!=="dark"){setTheme(getPreferredTheme())}});window.addEventListener("DOMContentLoaded",()=>{showActiveTheme(getPreferredTheme());document.querySelectorAll("[data-bs-theme-value]").forEach((toggle)=>{toggle.addEventListener("click",()=>{const theme=toggle.getAttribute("data-bs-theme-value");setStoredTheme(theme);setTheme(theme);showActiveTheme(theme,true)})})})})();
+(() => {
+  "use strict";
+
+  const getStoredTheme = () => localStorage.getItem("theme");
+  const setStoredTheme = (theme) => localStorage.setItem("theme", theme);
+
+  const setThemeIcon = (theme) => {
+    // 动态切换图标
+    const iconPathPrefix = `/images/${theme}/`;
+
+    // 替换所有具有相同类名的图标
+    const updateIcons = (className, iconName) => {
+      const icons = document.getElementsByClassName(className);
+      for (const icon of icons) {
+        icon.src = iconPathPrefix + iconName;
+      }
+    };
+
+    // 替换所有相关图标
+    updateIcons("right-icon", "right.svg");
+    updateIcons("clock-icon", "clock.svg");
+    updateIcons("chapter-icon", "chapter.svg");
+    updateIcons("download-icon", "download.svg");
+  };
+
+  const getPreferredTheme = () => {
+    const storedTheme = getStoredTheme();
+    setThemeIcon(storedTheme);
+
+    if (storedTheme) {
+      return storedTheme;
+    }
+
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+  };
+
+  const setTheme = (theme) => {
+    if (theme === "auto") {
+      document.documentElement.setAttribute(
+        "data-bs-theme",
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+          ? "dark"
+          : "light"
+      );
+    } else {
+      document.documentElement.setAttribute("data-bs-theme", theme);
+    }
+
+    setThemeIcon(theme);
+  };
+
+  setTheme(getPreferredTheme());
+
+  const showActiveTheme = (theme, focus = false) => {
+    const themeSwitcher = document.querySelector("#bd-theme");
+
+    if (!themeSwitcher) {
+      return;
+    }
+
+    const themeSwitcherText = document.querySelector("#bd-theme-text");
+    const activeThemeIcon = document.querySelector(".theme-icon-active use");
+    const btnToActive = document.querySelector(
+      `[data-bs-theme-value="${theme}"]`
+    );
+    const svgOfActiveBtn = btnToActive
+      .querySelector("svg use")
+      .getAttribute("href");
+
+    document.querySelectorAll("[data-bs-theme-value]").forEach((element) => {
+      element.classList.remove("active");
+      element.setAttribute("aria-pressed", "false");
+    });
+
+    btnToActive.classList.add("active");
+    btnToActive.setAttribute("aria-pressed", "true");
+    activeThemeIcon.setAttribute("href", svgOfActiveBtn);
+    const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`;
+    themeSwitcher.setAttribute("aria-label", themeSwitcherLabel);
+
+    if (focus) {
+      themeSwitcher.focus();
+    }
+  };
+
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", () => {
+      const storedTheme = getStoredTheme();
+      if (storedTheme !== "light" && storedTheme !== "dark") {
+        setTheme(getPreferredTheme());
+      }
+    });
+
+  window.addEventListener("DOMContentLoaded", () => {
+    showActiveTheme(getPreferredTheme());
+
+    document.querySelectorAll("[data-bs-theme-value]").forEach((toggle) => {
+      toggle.addEventListener("click", () => {
+        const theme = toggle.getAttribute("data-bs-theme-value");
+        setStoredTheme(theme);
+        setTheme(theme);
+        showActiveTheme(theme, true);
+      });
+    });
+  });
+})();
